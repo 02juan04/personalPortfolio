6fb36bd4afb84e24a8f1fae87e294a8d0f1645ba
02juan04 <romerojuan202@gmail.com>
2025-02-23 22:44:10 -0800

    Revert "test commit to scrallable"
    This reverts commit e3a5fe119ca991988706225b99668301d4b82cfc.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".navLink");
-    const currentPage = window.location.pathname.split("/").pop();
-  
-    links.forEach(link => {
-      if (link.getAttribute("href") === currentPage) {
-        link.classList.add("nav-link-active");
-      }
-    });
-  });
-  
+//function that takes an element and adds a class to it
+function addClass(element, className) {
+    element.classList.add(className);
+}
+
+//function that takes an element and removes a class from it
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
+//function that takes an element and toggles a class on it
+function toggleClass(element, className) {
+    element.classList.toggle(className);
+}
+
+//function that takes an element and checks if it has a class
+function hasClass(element, className) {
+    return element.classList.contains(className);
+}
+
+//function that checks if the nav linked was cliced and toggles the class on the nav
+function toggleNav() {
+    var navLink = document.getElementByClassName('navLink');
+    toggleClass(navLink, 'nav-link-cliked');
+}
+
+//listener that listens for a click on the nav link and calls the toggleNav function
+document.getElementById('navLink').addEventListener('click', toggleNav);
